6fefa2af41a3061f332760a3f4ca2adbc90ce370
Andrew <acullis68@gmail.com>
2019-06-19 15:35:15 +0200

    Fixed backup to only run mongo

--- index.js
+++ index.js
@@ -13,41 +13,33 @@ try {
 }
 
 let mongo_backup = require('./backup_mongo.js');
-let pg_backup = require('./backup_pg');
-let sf_backup = require('./backup_salesforce');
 
 function main(confObj) {
 
     let testConfig = {
         PATH_DATA_DUMPS : "./dumps",
         PATH_ZIP_DUMPS : "./archives",
-        PATH_DB_PREFIX_MONGO: "ablb_mongo_",
-        PATH_DB_PREFIX_SF: "ablb_salesforce_",
-        PATH_DB_PREFIX_PG: "ablb_postgres_"
+        PATH_DB_PREFIX_MONGO: "ablb_mongo_"
     };
 
     let config = confObj || testConfig;
 
-    console.log("Starting backup tasks...");
+    console.log("Starting backup...");
 
-    return Promise.all([
-        mongo_backup.backup(config, secrets),
-        pg_backup.backup(config, secrets),
-        // Sailsforce is no longer being used, and does not need to be updated
-        // sf_backup.backup(config, secrets, conn)
-    ]).then(successArr => {
-        console.log("All backups completed successfully!");
+    return mongo_backup.backup(config, secrets)
+    .then(success => {
+        console.log("All backup completed successfully!");
         return Promise.resolve("All files have been backed up!")
     }).catch(ex => {
         return Promise.reject(["Unable to backup some files!", ex]);
     })
 }
 
-// main().then(test => {
-//
-// }).catch(ex => {
-//     console.log(ex);
-// });
+main().then(test => {
+    console.log(test)
+}).catch(ex => {
+    console.log(ex);
+});
 
 module.exports = {
     backup: main
